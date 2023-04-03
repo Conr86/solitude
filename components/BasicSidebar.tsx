27456106ed7aae6ee7cfdb50e5eef4a800710016
Connor Kneebone <u6951141@anu.edu.au>
2023-04-04 00:37:56 +1000

    Added toggleable sidebar

--- components/BasicSidebar.tsx
+++ components/BasicSidebar.tsx
@@ -1,29 +1,27 @@
 import useSWR from 'swr'
 import { NavLink } from './NavLink'
-import { FaPlus, FaFile, FaHome, FaExclamationTriangle, FaTimes } from 'react-icons/fa'
+import { FaPlus, FaFile, FaHome, FaExclamationTriangle, FaTimes, FaChevronRight } from 'react-icons/fa'
 import { useRouter } from 'next/router'
 import { type Page } from '@prisma/client'
 import { useState } from 'react'
 
-export default function BasicSidebar (): JSX.Element {
-  const [filterText, setFilterText] = useState('')
-  const { data, error } = useSWR('/api/page')
-  const router = useRouter()
+export default function BasicSidebar(): JSX.Element {
+    const [filterText, setFilterText] = useState('')
+    const [sidebarVisible, setSidebarVisible] = useState(true);
+    const { data, error } = useSWR('/api/page')
+    const router = useRouter()
 
-  if (error) return <div>An error occured.</div>
+    if (error) return <div>An error occured.</div>
 
-  if (!data) return <div>Loading ...</div>
+    if (!data) return <div>Loading ...</div>
 
-  return (
+    return (
         <div>
-            <button data-drawer-target="sidebar-multi-level-sidebar" data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                <span className="sr-only">Open sidebar</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </button>
-
-            <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            <aside id="sidebar-multi-level-sidebar" className={`${sidebarVisible ? "translate-x-0" : "-translate-x-full"} fixed top-0 left-0 z-40 w-64 h-screen transition-transform duration-300 ease-in-out`} aria-label="Sidebar">
+                <button onClick={() => setSidebarVisible(!sidebarVisible)}
+                    className={`absolute bottom-2 -right-14 hover:bg-gray-200 text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-700 flex items-center p-4 rounded-lg group`}>
+                    <FaChevronRight className={`${sidebarVisible ? "rotate-180" : ""} transition ease-in-out duration-300`} />
+                </button>
                 <div className="h-full px-3 py-4 overflow-y-auto bg-secondary-100 dark:bg-secondary-800">
                     <ul className="space-y-2 font-medium">
                         <li>
@@ -43,7 +41,7 @@ export default function BasicSidebar (): JSX.Element {
                                 <input type="text" id="simple-search" value={filterText} onChange={e => { setFilterText(e.target.value) }}
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
                                 <button onClick={() => { setFilterText('') }}
-                                    className={`${filterText === '' ? 'hidden' : ''} absolute inset-y-0 right-0 text-gray-400 hover:text-gray-500 flex items-center pr-3`}><FaTimes/></button>
+                                    className={`${filterText === '' ? 'hidden' : ''} absolute inset-y-0 right-0 text-gray-400 hover:text-gray-500 flex items-center pr-3`}><FaTimes /></button>
                             </div>
                         </div>
                         {data.filter((item: Page) => item.title.toLowerCase().includes(filterText.toLowerCase())).map((page: Page) =>
@@ -70,5 +68,5 @@ export default function BasicSidebar (): JSX.Element {
                 </div>
             </aside>
         </div>
-  )
+    )
 }
