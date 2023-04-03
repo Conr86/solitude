82d85edccefa4944510493c08e5bf25282aa6401
Connor Kneebone <u6951141@anu.edu.au>
2023-04-03 18:04:42 +1000

    Added search (filtering) to sidebar

--- components/BasicSidebar.tsx
+++ components/BasicSidebar.tsx
@@ -1,10 +1,12 @@
 import useSWR from 'swr'
 import { NavLink } from './NavLink';
-import { FaPlus, FaFile, FaHome, FaExclamationTriangle } from 'react-icons/fa'
+import { FaPlus, FaFile, FaHome, FaExclamationTriangle, FaTimes } from 'react-icons/fa'
 import { useRouter } from 'next/router';
 import { Page } from '@prisma/client';
+import { useState } from 'react';
 
 export default function () {
+    const [filterText, setFilterText] = useState("");
     const { data, error } = useSWR('/api/page')
     const router = useRouter();
 
@@ -32,7 +34,19 @@ export default function () {
                         </li>
                     </ul>
                     <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
-                        {data.map((page: Page) =>
+                        <div className="flex items-center mb-4">
+                            <label htmlFor="simple-search" className="sr-only">Search</label>
+                            <div className="relative w-full">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                                </div>
+                                <input type="text" id="simple-search" value={filterText} onChange={e => setFilterText(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
+                                <button onClick={() => setFilterText("")} 
+                                    className={`${filterText == "" ? "hidden" : ""} absolute inset-y-0 right-0 text-gray-900  dark:text-gray-400 hover:text-gray-400 flex items-center pr-3`}><FaTimes/></button>
+                            </div>
+                        </div>
+                        {data.filter((item: Page) => item.title.toLowerCase().includes(filterText.toLowerCase())).map((page: Page) =>
                             <li>
                                 <NavLink href={page.id} Icon={FaFile} label={page.title} />
                             </li>
