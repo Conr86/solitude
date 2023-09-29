14ae91ed4586ea23570c38074274d40566246527
Connor Kneebone <u6951141@anu.edu.au>
2023-09-29 14:56:27 +1000

    Fix new page params

--- src/routes/new.tsx
+++ src/routes/new.tsx
@@ -1,10 +1,11 @@
-import Editor from '@/components/Editor.tsx'
+import Editor from "@/components/Editor.tsx";
 
 const NewPage = () => {
-    return (<>
-        <Editor title={"New Page"} content={""} createdAt={new Date()} id={0} updatedAt={new Date()} parentId={null} order={null}/>
-    </>
+    return (
+        <>
+            <Editor title={"New Page"} content={""} />
+        </>
     );
 };
 
-export default NewPage
+export default NewPage;
